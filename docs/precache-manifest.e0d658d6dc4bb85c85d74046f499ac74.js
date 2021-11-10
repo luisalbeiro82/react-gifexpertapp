808f5cd64ad58f8071aeafe189cb3e5b51f6db56
Luis Albeiro <luis.albeiro.h@gmail.com>
2021-11-09 22:00:22 -0500

    docs agregado

---
+++ docs/precache-manifest.e0d658d6dc4bb85c85d74046f499ac74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ebb14d036b1db77893a1adccfead1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cf10c1771a960f182ca",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cf10c1771a960f182ca",
+    "url": "/static/js/main.e4816d6d.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
